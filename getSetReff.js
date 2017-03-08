2344be7a00df9fd69d64557451943964754d740c
Peter <peter@barschool.net>
2017-03-08 10:05:20 +0700

    Fixed issue in logic around gclid and reformatted code for readablity

--- getSetReff.js
+++ getSetReff.js
@@ -1,12 +1,8 @@
-function getSetReff()
-{
-
-    if (!Array.prototype.filter) //Array.filter() isn't included in IE until version 9.
-    {
-      Array.prototype.filter = function(fun /*, thisArg */)
-      {
+function getSetReff(){
+    //Array.filter() isn't included in IE until version 9.
+    if (!Array.prototype.filter){
+      Array.prototype.filter = function(fun /*, thisArg */){
         "use strict";
-
         if (this === void 0 || this === null)
           throw new TypeError();
 
@@ -17,10 +13,8 @@ function getSetReff()
 
         var res = [];
         var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
-        for (var i = 0; i < len; i++)
-        {
-          if (i in t)
-          {
+        for (var i = 0; i < len; i++){
+          if (i in t){
             var val = t[i];
 
             // NOTE: Technically this should Object.defineProperty at
@@ -40,11 +34,26 @@ function getSetReff()
     var _reff=[];
     _reff = dataLayer.filter(function (value) {if (value.setDomain)  return value; });
     if (_reff.length === 0) _reff[0]={'setDomain':document.location.hostname};
+
+    function rC(k){
+      return(document.cookie.match('(^|; )'+k+'=([^;]*)')||0)[2]
+    }
     
-    function rC(k){return(document.cookie.match('(^|; )'+k+'=([^;]*)')||0)[2]}
-    function sC(n,v,d) { dd = new Date(); dd.setTime(dd.getTime() + (d*24*60*60*1000)) ;e = typeof d != "undefined" ? ";expires="+dd.toGMTString() : ""; document.cookie = n+"="+v+e+";domain="+_reff[0].setDomain+";path=/"; }
-    function gcP(p) { if (document.location.search.indexOf(p) != -1) { return (""+document.location.search.split(p+"=")[1]).split("&")[0]; } else { return "not-set"; }  }
-    
+    function sC(n,v,d) {
+      dd = new Date();
+      dd.setTime(dd.getTime() + (d*24*60*60*1000));
+      e = typeof d != "undefined" ? ";expires="+dd.toGMTString() : "";
+      document.cookie = n+"="+v+e+";domain="+_reff[0].setDomain+";path=/";
+    }
+
+    function gcP(p) {
+      if (document.location.search.indexOf(p) != -1) {
+         return (""+document.location.search.split(p+"=")[1]).split("&")[0];
+      } else {
+        return "not-set";
+      }
+    }
+
     //every pageview
     var __asc = (typeof rC("__sreff") != "undefined" ? rC("__sreff") : "");
     var __apc = (typeof rC("__reff") != "undefined" ? rC("__reff") : "");
@@ -59,53 +68,43 @@ function getSetReff()
     if (document.location.search.indexOf(__cmp) != -1 || document.location.search.indexOf(__mdm) != -1 || document.location.search.indexOf(__srcs) != -1){
       __gsr = "//campaign::c:["+gcP(__cmp)+"]m:["+gcP(__mdm)+"]s:["+gcP(__srcs)+"]";
       __gsr += (document.location.search.indexOf("gclid") != -1) ? "g:["+gcP("gclid")+"]" : '';
-    } else if (document.location.search.indexOf("gclid") != -1){
-      __gsr = "//campaign::[adwords]";
+    } else if (document.location.search.indexOf("gclid") != -1) {
+        __gsr = "//campaign::[adwords]" + "g:["+gcP("gclid")+"]";
     } else {
       __gsr = document.referrer;
     }
+
     __gsr = ((typeof __gsr == "undefined" || __gsr == "" || __gsr.indexOf(_reff[0].setDomain)!=-1) ? "(direct)" : __gsr.split('/')[2]);
 
-    if (__asc)
-    {
-        __tsc = __asc.split(".");
-        __tsc[1] = new Date().getTime();
-        __tsc[2]++;
-        __asc = __tsc.join(".");
-        sC("__sreff",__asc);
+    if (__asc){
+      __tsc = __asc.split(".");
+      __tsc[1] = new Date().getTime();
+      __tsc[2]++;
+      __asc = __tsc.join(".");
+      sC("__sreff",__asc);
+    } else {
+      __tsc[0] = __tsc[1] = new Date().getTime(); //start time = current time
+      __tsc[2]=1; //first pageview
+      __asc = __tsc.join(".");
+      __nwss = 1;
+      sC("__sreff",__asc);
     }
-    else
-    {
-        __tsc[0] = __tsc[1] = new Date().getTime(); //start time = current time
-        __tsc[2]=1; //first pageview
-        __asc = __tsc.join(".");
-        __nwss = 1;
-        sC("__sreff",__asc);
-    }
-    
-
 
     //if new session
-    if (__nwss == 1)
-    {
-        res__apc = (__apc != "" ? __apc+"|" : ""); 
-        sC("__reff",res__apc+__gsr+"&"+__asc,730);
-    }
-    else
-    {
-        t__apc = __apc.split("|");
-        __tpc = t__apc[t__apc.length-1];
-        __tpc=__tpc.split("&")[0] != "" ? __tpc.split("&")[0] : __gsr;
-        res__apc = (t__apc.length == 1 ? "" : (t__apc.slice(0,-1).join("|")+"|"));
+    if (__nwss == 1){
+      res__apc = (__apc != "" ? __apc+"|" : "");
+      sC("__reff",res__apc+__gsr+"&"+__asc,730);
+    } else {
+      t__apc = __apc.split("|");
+      __tpc = t__apc[t__apc.length-1];
+      __tpc=__tpc.split("&")[0] != "" ? __tpc.split("&")[0] : __gsr;
+      res__apc = (t__apc.length == 1 ? "" : (t__apc.slice(0,-1).join("|")+"|"));
 
-        if (__gsr == "(direct)" || __gsr.indexOf(__tpc.split("&")[0])!=-1) 
-        {
-            sC("__reff",res__apc+__tpc+"&"+__asc,730);
-        }
-        else
-        {
-            sC("__reff",__apc+"|"+__gsr+"&"+__asc,730);
-        } 
+      if (__gsr == "(direct)" || __gsr.indexOf(__tpc.split("&")[0])!=-1){
+        sC("__reff",res__apc+__tpc+"&"+__asc,730);
+      } else {
+        sC("__reff",__apc+"|"+__gsr+"&"+__asc,730);
+      }
     }
     return rC("__reff");
 }
